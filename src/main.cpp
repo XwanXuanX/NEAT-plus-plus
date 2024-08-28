74276e3c071b958b41244b2de35b27c771ea3ebc
zicongleoqu <leoqu9577@gmail.com>
2024-08-28 07:53:43 -0400

    Call generate image and path fix(please verify)

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,12 @@ int main(){
         // testing genotype.dump method
         Genotype g(3, 2);
         g.dump();
+        g.generate_image();
 
         // testing genotype.constructor
-        Genotype t("../build/1.model");
+        Genotype t("1.model");
         t.dump();
+        t.generate_image();
         
         return 0;
 }
