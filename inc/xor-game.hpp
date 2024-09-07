105cbce583e8cbb6536b6386f07a3e0f44cb4516
Yetong <t378li@uwaterloo.ca>
2024-09-07 17:34:42 +0800

    update xor-game based on new eval-interval

--- inc/xor-game.hpp
+++ inc/xor-game.hpp
@@ -9,12 +9,17 @@ class XorGame : public EvalInterface{
         [[nodiscard]] explicit XorGame(const std::uint32_t in_pin);
 
     private: // private member functions
+        // initialize the genotype's fitness to 0
+        virtual void initialize(Genotype& geno) override final;
+
         // generate random bits as inputs to the xor gate
         virtual DataPkt collect() const override final;
+
         // check if the produced output matches the expected output, and end the game
         virtual bool acturate(const DataPkt& pkt) override final;
+        
         // increase the score if the output is correct
-        virtual int64_t upd_score(const int64_t old_score) const override final;
+        virtual long double upd_score(const long double old_score) const override final;
 
     private: // private member variables
         const std::uint32_t in_pin;
