41d0727752c4ee10a52022560c8441be6bbb3272
Yetong <t378li@uwaterloo.ca>
2024-09-08 17:29:11 -0400

    XorGame testcase

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,23 @@
 #include <iostream>
 #include "genotype.hpp"
 #include "prob.hpp"
+#include "xor-game.hpp"
 
 int main(){
         // testing genotype.dump method
         Genotype g(3, 2);
         GenotypeProbing::dump(g);
-        GenotypeProbing::generate_image(g);
+        // GenotypeProbing::generate_image(g);
 
         // testing genotype.constructor
         Genotype t("1.model");
         GenotypeProbing::dump(t);
-        GenotypeProbing::generate_image(t);
-        
+        // GenotypeProbing::generate_image(t);
+
+        // testing the XorGame
+        XorGame game(2);
+        game.loop(g);
+        std::cout << g.fitness << std::endl;
+
         return 0;
 }
