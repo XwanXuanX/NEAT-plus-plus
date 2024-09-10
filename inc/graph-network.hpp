3516921931a917df10d537c8875c28d4765fc9fa
Yetong <t378li@uwaterloo.ca>
2024-09-10 09:55:47 -0400

    class definition of the graph-base network

---
+++ inc/graph-network.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <map>
+#include <set>
+#include <list>
+#include <vector>
+#include <cstdint>
+
+// forward class declarations
+struct Connection;
+
+// ASSUME ALL GRAPHS ARE DIRECTED!
+class GraphNet{
+    public:
+        // map is a set of pairs itself; easier to write, have move functionalities,
+        // and two level indexing makes updating an edge weight O(2logN)
+        using WeightedGraph = std::map<uint64_t, std::map<uint64_t, long double>>;
+        using UnweightedGraph = std::map<uint64_t, std::set<uint64_t>>;
+        using ConnectionList = std::list<Connection>;
+        using NodeID = const std::uint64_t;
+
+        // construct both graphs from list of connections
+        void construct(const ConnectionList& connections);
+
+        // add an edge to both graphs - if edge already exists, return false
+        bool add(NodeID in_node, NodeID out_node, const long double weight);
+
+        // erase an edge from both graphs - if edge does not exist, return false
+        bool erase(NodeID in_node, NodeID out_node);
+
+        // find all ancestors that can reach the target node via at least one path
+        std::set<uint64_t> ancestors(NodeID node) const;
+
+        // find all children that is reachable from the target node via at least one path
+        std::set<uint64_t> children(NodeID node) const;
+
+        // return the topological ordering of the WEIGHTED graph
+        std::vector<uint64_t> topsort() const;
+
+        // check if there exists a cycle in the WEIGHTED graph
+        bool has_cycle() const;
+
+        // count the number of connected components in the WEIGHTED graph
+        uint64_t components() const;
+
+    private:
+        // adjacency list strcture of the network
+        WeightedGraph graph;
+        UnweightedGraph Tgraph;
+};
