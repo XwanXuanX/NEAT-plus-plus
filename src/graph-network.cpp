b9fc5f53f4f645111af219a20007f9a31a828ec3
Yetong <t378li@uwaterloo.ca>
2024-09-10 14:18:28 -0400

    top sort method

--- src/graph-network.cpp
+++ src/graph-network.cpp
@@ -1,6 +1,9 @@
 #include "graph-network.hpp"
+#include "utility.hpp"
 #include <utility>
 #include <cassert>
+#include <stdexcept>
+#include <deque>
 
 // construct both graphs from list of connections
 void GraphNet::construct(const GraphNet::ConnectionList& connections){
@@ -49,7 +52,40 @@ std::set<uint64_t> GraphNet::children(NodeID node) const{
 
 // return the topological ordering of the WEIGHTED graph
 std::vector<uint64_t> GraphNet::topsort() const{
+        // this method use Kahn's algorithm for topological ordering
+        std::vector<uint64_t> indeg(node_count + 1, 0); // node id start from 1
+        // calculate the in degree of each vertex
+        for(auto& [in, outs] : graph)
+                for(auto& out : outs)
+                        indeg.at(out.first)++;
+        
+        // obtain all the nodes with in degree 0
+        std::deque<uint64_t> q;
+        for(uint64_t node = 1; node <= node_count; ++node)
+                if(indeg[node] == 0)
+                        q.push_back(node);
 
+        // performing BFS        
+        std::vector<uint64_t> res;
+        while(!q.empty()){
+                uint64_t node = q.front();
+                q.pop_front();
+                res.push_back(node);
+                // decrease the degree of adjacent nodes; if no adjacent nodes (ie. output nodes) skip
+                if(!graph.count(node))
+                        continue;
+                for(auto& adj : graph.at(node)){
+                        indeg.at(adj.first)--;
+                        if(indeg.at(adj.first) == 0)
+                                q.push_back(adj.first);
+                }
+        }
+
+        // check for a cycle
+        if(res.size() != node_count)
+                throw std::runtime_error(make_errmsg(__FILE__,__LINE__,"graph contains cycle(s)!"));
+        
+        return res;
 }
 
 // check if there exists a cycle in the WEIGHTED graph
@@ -67,48 +103,3 @@ bool GraphNet::exist(NodeID in_node, NodeID out_node) const{
         return (graph.count(in_node) && // in node must first exists and out node exists as a child of in node
                 graph.at(in_node).count(out_node));
 }
-        
-
-
-
-
-
-#if false
-// helper method to generate the topological ordering of the graph
-auto Genotype::top_sort() const -> std::vector<uint64_t>{
-        // this method use Kahn's algorithm for topological ordering
-
-        std::vector<uint64_t> indeg(node_genes.size() + 1, 0); // node id start from 1
-        // calculate the in degree of each vertex
-        for(auto& connection : connection_genes)
-                if(connection.enable)
-                        indeg[connection.out]++;
-        
-        // obtain all the nodes with in degree 0
-        std::deque<uint64_t> q;
-        for(auto& node : node_genes)
-                if(indeg[node.node_number] == 0)
-                        q.push_back(node.node_number);
-        
-        std::vector<uint64_t> res;
-        while(!q.empty()){
-                uint64_t node = q.front();
-                q.pop_front();
-                res.push_back(node);
-                // decrease the degree of adjacent nodes; if no adjacent nodes (ie. output nodes) skip
-                if(!graph.count(node))
-                        continue;
-                for(auto& adj : graph.at(node)){
-                        indeg[adj.first]--;
-                        if(indeg[adj.first] == 0)
-                                q.push_back(adj.first);
-                }
-        }
-
-        // check for a cycle
-        if(res.size() != node_genes.size())
-                throw std::runtime_error(make_errmsg(__FILE__,__LINE__,"graph contains cycle(s)!"));
-
-        return res;
-}
-#endif
